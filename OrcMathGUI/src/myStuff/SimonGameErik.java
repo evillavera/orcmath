17abac568e0ae5f54ec066834bb9386e8b80497a
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2018-01-03 10:47:03 -0500

    fixing stuff

---
+++ OrcMathGUI/src/myStuff/SimonGameErik.java
@@ -0,0 +1,14 @@
+package myStuff;
+
+public class SimonGameErik {
+
+	public SimonGameErik() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
